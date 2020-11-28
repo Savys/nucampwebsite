d9c8ce8f23f1cf425d08681db23906a26c0c9a4d
Savitha Srinivasan <savysree2@gmail.com>
2020-11-28 15:54:10 -0800

    Week4:Assignment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+    
     $(".carousel").carousel( { interval: 2000 } );
     $("#carouselButton").click(function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
@@ -11,4 +12,13 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+    
+            
+        $("#reserveButton").click(function(){
+            $('#reserveModal').modal('show');
+        })
+        $("#loginButton").click(function(){
+            $('#loginModal').modal('show');
+        })
+    
 });
